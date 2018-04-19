e7462b9263d6c337319a72c106bc80d43bef61a8
Hou <houlxssdut@gmail.com>
2018-04-19 20:22:37 +0800

    Public Service Address Book

---
+++ AddrBook/app/src/main/java/com/ssdut/houlx/addrbook/SplashActivity.java
@@ -0,0 +1,57 @@
+package com.ssdut.houlx.addrbook;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobUser;
+
+/**
+ * @author houlx
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int GO_SIGNUP = 1;
+    private static final int GO_HOME = 2;
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what) {
+                case GO_SIGNUP:
+                    Intent intent = new Intent(SplashActivity.this, SignUpActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+                case GO_HOME:
+                    Intent intent1 = new Intent(SplashActivity.this, ContactsActivity.class);
+                    startActivity(intent1);
+                    finish();
+                    break;
+                default:
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        Bmob.initialize(this, "381f45436aabb971eadd32ec2470cf3a");
+
+        BmobUser user = BmobUser.getCurrentUser();
+        if (user != null) {
+            handler.sendEmptyMessageDelayed(GO_HOME, 2000);
+        } else {
+            handler.sendEmptyMessageDelayed(GO_SIGNUP, 2000);
+        }
+    }
+}
